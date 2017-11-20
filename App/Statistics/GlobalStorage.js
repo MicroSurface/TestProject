39b16d45c4388108af304bcf5885fea559070513
MicroSurface <zx2013rt@outlook.com>
2017-11-20 23:49:13 +0800

    TestProject_Show the header image after choosing image
    TestProject_Show the header image after choosing image

--- App/Statistics/GlobalStorage.js
+++ App/Statistics/GlobalStorage.js
@@ -5,6 +5,13 @@ global.user = {
 	userData:'',
 };
 
+global.headerImage = {
+	//是否上传头像
+	hasLoaded:'',
+	//是否有头像路径
+	imagePath:'',
+}
+
 //进入App时检查是否存在数据并设置状态；
 storage.load({
 	key:'loginState',
@@ -14,4 +21,14 @@ storage.load({
 }).catch(err => {
 	global.user.loginState = false;
 	global.user.userData = '';
+})
+
+storage.load({
+	key:'headerImage',
+}).then(ret => {
+	global.headerImage.hasLoaded = true;
+	global.headerImage.imagePath = ret;
+}).catch(err => {
+	global.headerImage.hasLoaded = false;
+	global.headerImage.imagePath = '';
 })
